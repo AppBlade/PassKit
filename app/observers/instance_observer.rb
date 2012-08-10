29b3913d353df33609d3b6b1edf578f4e206db00
Michele <michele.titolo@raizlabs.com>
2012-08-10 14:28:15 -0700

    Removed not working instance observer code.

--- app/observers/instance_observer.rb
+++ app/observers/instance_observer.rb
@@ -1,7 +1,8 @@
 class InstanceObserver < ActiveRecord::Observer
 
 	def after_update(instance)	
-		InstanceMailer.instance_updated(instance.id).deliver!
+		# This is wrong. We need a better way to update users about updates. 
+		# InstanceMailer.instance_updated(instance.id).deliver!
 	end
 
 end
